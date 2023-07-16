8a17125bd11a71c17d8eaab66cfc6cb98afcdd74
pixlmint <christian.groeber@bluewin.ch>
2023-07-16 21:12:54 +0200

    wip: started work on indexer

--- config/routes.php
+++ config/routes.php
@@ -3,6 +3,7 @@
 use PixlMint\JournalPlugin\Controllers\AdminController;
 use PixlMint\JournalPlugin\Controllers\CacheController;
 use PixlMint\JournalPlugin\Controllers\EntriesController;
+use PixlMint\WikiPlugin\Controllers\IndexingController;
 use PixlMint\WikiPlugin\Controllers\NavController;
 
 return [
@@ -10,5 +11,10 @@ return [
         'route' => '/api/nav',
         'controller' => NavController::class,
         'function' => 'loadNav',
+    ],
+    [
+        'route' => '/api/index',
+        'controller' => IndexingController::class,
+        'function' => 'index',
     ],
 ];
---
+++ src/Controllers/IndexingController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace PixlMint\WikiPlugin\Controllers;
+
+use Nacho\Controllers\AbstractController;
+use PixlMint\WikiPlugin\Helpers\Indexer;
+
+class IndexingController extends AbstractController
+{
+    public function index(): string
+    {
+        $indexer = new Indexer();
+        $index = $indexer->indexDb($this->nacho);
+
+        return $this->json($index);
+    }
+}
