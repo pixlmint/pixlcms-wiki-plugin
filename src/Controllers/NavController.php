5fd02a2d30758d4721b7e21030e14b51a75c46c9
ChristianGroeber <swiss8oy.chg@gmail.com>
2024-06-13 16:17:09 +0200

    allow force-reloading nav

--- src/Controllers/NavController.php
+++ src/Controllers/NavController.php
@@ -2,14 +2,19 @@
 
 namespace PixlMint\WikiPlugin\Controllers;
 
+use Nacho\Contracts\RequestInterface;
 use Nacho\Controllers\AbstractController;
 use Nacho\Models\HttpResponse;
 use PixlMint\WikiPlugin\Helpers\NavRenderer;
 
 class NavController extends AbstractController
 {
-    public function loadNav(NavRenderer $navRenderer): HttpResponse
+    public function loadNav(NavRenderer $navRenderer, RequestInterface $request): HttpResponse
     {
-        return $this->json($navRenderer->loadNav());
+        $forceRerender = false;
+        if ($request->getBody()->has('forceReload')) {
+            $forceRerender = true;
+        }
+        return $this->json($navRenderer->loadNav([], $forceRerender));
     }
 }
