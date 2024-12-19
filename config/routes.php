66b0dc707b39dc4609ace98bce4fd4e36a57cf06
ChristianGroeber <swiss8oy.chg@gmail.com>
2024-12-19 22:42:49 +0100

    migrate nav functionality from wiki plugin into cms core

--- config/routes.php
+++ config/routes.php
@@ -1,19 +1,10 @@
 <?php
 
-use PixlMint\JournalPlugin\Controllers\AdminController;
-use PixlMint\JournalPlugin\Controllers\CacheController;
-use PixlMint\JournalPlugin\Controllers\EntriesController;
 use PixlMint\WikiPlugin\Controllers\IndexingController;
-use PixlMint\WikiPlugin\Controllers\NavController;
 use PixlMint\WikiPlugin\Controllers\SearchController;
 use PixlMint\WikiPlugin\Controllers\SvgController;
 
 return [
-    [
-        'route' => '/api/nav',
-        'controller' => NavController::class,
-        'function' => 'loadNav',
-    ],
     [
         'route' => '/api/index',
         'controller' => IndexingController::class,
